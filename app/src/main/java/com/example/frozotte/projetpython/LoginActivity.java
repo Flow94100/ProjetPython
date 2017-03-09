7a7260866f2d00ac30ca23c477d465e59fb510c1
Flo <rozotte.florian@gmail.com>
2017-03-09 16:51:31 +0100

    Commit de fin de journée

--- app/src/main/java/com/example/frozotte/projetpython/LoginActivity.java
+++ app/src/main/java/com/example/frozotte/projetpython/LoginActivity.java
@@ -7,13 +7,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button btnConnexion;
     TextView txtSkip;
     TextView txtInscri;
+    EditText editLog;
+    EditText editMdp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,8 @@ public class LoginActivity extends AppCompatActivity {
         txtSkip = (TextView)findViewById(R.id.txtSkip);
         txtInscri = (TextView)findViewById(R.id.txtInscri);
         btnConnexion = (Button)findViewById(R.id.btnConn);
+        editLog = (EditText)findViewById(R.id.editText);
+        editMdp = (EditText)findViewById(R.id.editText2);
 
         //Si pas inscri, passer au fil d'actu directement
         txtSkip.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +47,19 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent iInscri = new Intent(getApplication(),InscriptionActivity.class);
                 startActivity(iInscri);
+            }
+        });
+
+        btnConnexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editLog.getText().toString().equals("admin") && editMdp.getText().toString().equals("admin")){
+                    Intent iActu = new Intent(getApplicationContext(),AccueilActivity.class);
+                    startActivity(iActu);
+                    finish();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Utilisateur inconnu", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
