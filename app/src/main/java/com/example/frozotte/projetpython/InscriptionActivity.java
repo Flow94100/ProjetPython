0605522ef5d6efd6dc5fdffdeb0c10f9d38c15ae
Flo <rozotte.florian@gmail.com>
2017-03-09 10:35:45 +0100

    Ajout des activités

---
+++ app/src/main/java/com/example/frozotte/projetpython/InscriptionActivity.java
@@ -0,0 +1,15 @@
+package com.example.frozotte.projetpython;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class InscriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inscription);
+
+    }
+}
